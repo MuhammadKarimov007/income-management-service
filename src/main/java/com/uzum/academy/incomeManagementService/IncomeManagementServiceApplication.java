f8a5115894d2ca7abe5e102e3b3deb5db8655556
Muhammad Karimov <shermuhammad.karimov2004@mail.ru>
2024-07-09 21:20:21 +0500

    initial commit

---
+++ src/main/java/com/uzum/academy/incomeManagementService/IncomeManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uzum.academy.incomeManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomeManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uzum/academy/incomeManagementService/IncomeManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzum.academy.incomeManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
