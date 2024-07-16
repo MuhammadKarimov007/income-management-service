b33086cbf4889ced9950aaca3509417af9cd0479
Muhammad Karimov <shermuhammad.karimov2004@mail.ru>
2024-07-16 20:42:43 +0500

    created home endpoint

--- src/main/java/com/uzum/academy/incomeManagementService/Application.java
+++ src/main/java/com/uzum/academy/incomeManagementService/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IncomeManagementServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IncomeManagementServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
